f97ff3301ae8903ca8e301bdc10c8b2144fd1315
evelyn345 <tibia_8_20@hotmail.com>
2017-05-24 23:03:29 -0600

    Add files via upload

---
+++ vidas.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vidas : MonoBehaviour
+{
+
+    public GameObject cronometroGO;
+    public Cronometro cronometroScript;
+
+    private void Start()
+    {
+        cronometroGO = GameObject.FindObjectOfType<Cronometro>().gameObject;
+        cronometroScript = cronometroGO.GetComponent<Cronometro>();
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Tank>() != null)
+        {
+            cronometroScript.lifes = cronometroScript.lifes + 1;
+            Destroy(this.gameObject);
+
+        }
+    }
+}
